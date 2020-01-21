ada99a30d7d3c64f21c642bb26096b1d926cec4c
Bota5ky <zjy2516@126.com>
2020-01-21 09:12:22 +0800

    6 和 9 组成的最大数字

---
+++ 1323.go
@@ -0,0 +1,17 @@
+package temp
+//maximum-69-number
+import (
+	"strconv"
+)
+func maximum69Number(num int) int {
+	n := strconv.Itoa(num)
+	ret := []byte(n)
+	for i := 0; i < len(ret); i++ {
+		if ret[i] == '6' {
+			ret[i] = '9'
+            break
+		}
+	}
+	x, _ := strconv.Atoi(string(ret))
+	return x
+}
