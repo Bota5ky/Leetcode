9ac4c5013b45fee63bcec56dcc092aebc7da6184
Bota5ky <zjy2516@126.com>
2020-01-22 16:02:31 +0800

    除自身以外数组的乘积

---
+++ 238.go
@@ -0,0 +1,16 @@
+package temp
+
+//product-of-array-except-self
+func productExceptSelf(nums []int) []int {
+	ret := make([]int, len(nums))
+	ret[0] = 1
+	for i := 0; i < len(nums)-1; i++ {
+		ret[i+1] = ret[i] * nums[i]
+	}
+	temp := 1
+	for i := len(nums) - 2; i >= 0; i-- {
+		temp *= nums[i+1]
+		ret[i] *= temp
+	}
+	return ret
+}
