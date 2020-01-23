a2a6d3982dfa0eae47e3dd7cc939528b40ffc0a8
Bota5ky <zjy2516@126.com>
2020-01-23 09:55:52 +0800

    重复叠加字符串匹配

--- 686.go
+++ 686.go
@@ -8,11 +8,11 @@ func repeatedStringMatch(A string, B string) int {
 	cnt := 1
 	C := A
 	for len(C) <= maxlen {
-		if strings.Contains(C, B) {
-			return cnt
-		} else {
+		if strings.Contains(C, B)==false {
 			C += A
 			cnt++
+		} else {
+			return cnt
 		}
 	}
 	return -1
