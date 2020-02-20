278eea0d7e68b15138e1b159a65773c02e5f9a17
Bota5ky <zjy2516@126.com>
2020-02-20 18:53:33 +0800

    面试题40. 最小的k个数

---
+++ offer40.go
@@ -0,0 +1,12 @@
+package temp
+
+func getLeastNumbers(arr []int, k int) []int {
+	for i := k; i > 0; i-- {
+		for j := len(arr) - 2; j >= 0; j-- {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr[:k]
+}
