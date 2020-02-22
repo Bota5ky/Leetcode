9f2d29adc5716502c386738aa9f1899a37f3787b
Bota5ky <zjy2516@126.com>
2020-02-22 10:41:52 +0800

    面试题14- II. 剪绳子 II

---
+++ offer14-II.go
@@ -0,0 +1,17 @@
+package temp
+
+func cuttingRope2(n int) int {
+    switch n {
+	case 2:
+		return 1
+	case 3:
+		return 2
+	}
+	mul := 1
+	for n > 4 {
+		n -= 3
+		mul *= 3
+        if mul>1000000007 {mul%=1000000007}
+	}
+	return mul * n % 1000000007
+}
