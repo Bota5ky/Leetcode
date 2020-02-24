25b5cba50be1e2a60cd479c02094e527193fac48
Bota5ky <zjy2516@126.com>
2020-02-24 21:43:45 +0800

    面试题26. 树的子结构

---
+++ offer26.go
@@ -0,0 +1,37 @@
+package temp
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	//遍历判断是否是相同节点
+	if A == nil {
+		return B == nil
+	}
+	if B == nil {
+		return false
+	}
+	//子树是否相同
+	if isSameTree(A, B) {
+		return true
+	}
+	return isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
+}
+
+func isSameTree(A *TreeNode, B *TreeNode) bool {
+	if A == nil {
+		return B == nil
+	}
+	if B == nil {
+		return true
+	}
+	if A.Val != B.Val {
+		return false
+	}
+	return isSameTree(A.Left, B.Left) && isSameTree(A.Right, B.Right)
+}
