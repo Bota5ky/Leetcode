ae613887b5d6a9d8cb3098ad233ec202e28e1868
Bota5ky <zjy2516@126.com>
2020-02-25 20:47:30 +0800

    面试题32 - II. 从上到下打印二叉树 II

---
+++ offer32-II.go
@@ -0,0 +1,23 @@
+package temp
+
+func levelOrder(root *TreeNode) [][]int {
+	var ret [][]int
+	j := 0
+	stack := []*TreeNode{root}
+	for len(stack) != 0 {
+		k := len(stack)
+		flag:=0
+		for i := 0; i < k; i++ {
+			if stack[i]==nil {continue}
+			if flag==0 {
+				ret=append(ret,[]int{})
+				flag=1
+			}
+			ret[j] = append(ret[j], stack[i].Val)
+			stack = append(stack, stack[i].Left, stack[i].Right)
+		}
+		stack = stack[k:]
+		j++
+	}
+	return ret
+}
