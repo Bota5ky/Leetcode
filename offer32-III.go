a43faee9b6ab3ca70fec6d209317e127a20e2988
Bota5ky <zjy2516@126.com>
2020-02-25 21:20:14 +0800

    面试题32 - III. 从上到下打印二叉树 III

--- offer32-III.go
+++ offer32-III.go
@@ -1,24 +1,28 @@
 package temp
 
-func levelOrder3(_)(root *TreeNode) [][]int {
+func levelOrder3(root *TreeNode) [][]int {
 	var ret [][]int
 	stack := []*TreeNode{root}
-	j:=0
+	j := 0
 	for len(stack) != 0 {
 		k := len(stack)
 		flag := 0
 		for i := 0; i < k; i++ {
 			if stack[i] != nil {
-				if flag==0 {
-					ret=append(ret,[]int{})
-					flag=1
+				if flag == 0 {
+					ret = append(ret, []int{})
+					flag = 1
 				}
-				if j%2==0 {ret[j]=append(ret[j],stack[i].Val)}else{ret[j]=append([]int{stack[i].Val},ret[j]...)}
-				stack=append(stack,stack[i].Left,stack[i].Right)
+				if j%2 == 0 {
+					ret[j] = append(ret[j], stack[i].Val)
+				} else {
+					ret[j] = append([]int{stack[i].Val}, ret[j]...)
+				}
+				stack = append(stack, stack[i].Left, stack[i].Right)
 			}
 		}
-		stack=stack[k:]
+		stack = stack[k:]
 		j++
-    }
+	}
 	return ret
 }
