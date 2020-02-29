90a3be0629bfb61201b6604635185c8d4b80b26b
Bota5ky <zjy2516@126.com>
2020-02-29 18:21:20 +0800

    最大数

---
+++ 179.go
@@ -0,0 +1,37 @@
+package temp
+
+import "strconv"
+
+//和offer45类似
+func largestNumber(nums []int) string {
+	var strNums []string
+	for i := 0; i < len(nums); i++ {
+		strNums = append(strNums, strconv.Itoa(nums[i]))
+		for j := len(strNums) - 2; j >= 0; j-- {
+			if cmpStrNums2(strNums[j], strNums[j+1]) {
+				strNums[j], strNums[j+1] = strNums[j+1], strNums[j]
+			}
+		}
+	}
+	var ret string
+	for i := 0; i < len(strNums); i++ {
+		ret += strNums[i]
+		if i == 0 && ret == "0" {
+			break
+		}
+	}
+	return ret
+}
+
+func cmpStrNums2(a string, b string) bool {
+	m := a + b
+	n := b + a
+	for i := 0; i < len(m); i++ {
+		if m[i] < n[i] {
+			return true
+		} else if m[i] > n[i] {
+			return false
+		}
+	}
+	return false
+}
