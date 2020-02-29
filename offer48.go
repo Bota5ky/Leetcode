48db2a0d31e2f83c321d7f6dc3f704a34ecb62a9
Bota5ky <zjy2516@126.com>
2020-02-29 20:44:16 +0800

    面试题48. 最长不含重复字符的子字符串

---
+++ offer48.go
@@ -0,0 +1,27 @@
+package temp
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	strMap := make(map[byte]int)
+	strMap[s[0]] = 1 //位置在strMap[s[0]]-1
+	head := 0
+	rear := 1
+	max := 1
+	for rear < len(s) {
+		if strMap[s[rear]] > 0 {
+			if head < strMap[s[rear]] {
+				head = strMap[s[rear]]
+			}
+			strMap[s[rear]] = rear + 1
+		} else {
+			strMap[s[rear]] = rear + 1
+		}
+		if rear-head+1 > max {
+			max = rear - head + 1
+		}
+		rear++
+	}
+	return max
+}
