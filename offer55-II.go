33d776f895a001e522838687407a0b0bf7b8fc4a
Bota5ky <zjy2516@126.com>
2020-03-01 16:15:13 +0800

    面试题55 - II. 平衡二叉树

--- offer55-II.go
+++ offer55-II.go
@@ -31,3 +31,27 @@ func depthOfTreeNode(root *TreeNode) int {
 	}
 	return 1 + right
 }
+
+//后序遍历只需遍历一次
+// func isBalanced(root *TreeNode) bool {
+// 	var depth int
+// 	return isBalancedTree(root, &depth)
+// }
+// func isBalancedTree(root *TreeNode, depth *int) bool {
+// 	if root == nil {
+// 		*depth = 0
+// 		return true
+// 	}
+// 	var left, right int
+// 	if isBalancedTree(root.Left, &left) && isBalancedTree(root.Right, &right) {
+// 		if left >= right {
+// 			*depth = 1 + left
+// 		} else {
+// 			*depth = 1 + right
+// 		}
+// 		if left-right <= 1 && left-right >= -1 {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
