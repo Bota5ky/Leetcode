57c549a26366fa48e2565d4abb5c44413f55255b
Bota5ky <zjy2516@126.com>
2020-03-01 20:12:46 +0800

    面试题57 - II. 和为s的连续正数序列

---
+++ offer57-II.go
@@ -0,0 +1,27 @@
+package temp
+
+func findContinuousSequence(target int) [][]int {
+	var ret [][]int
+	i, j := 1, 2
+	sum := 3
+	column := []int{i, j}
+	for i <= target/2 {
+		if sum > target {
+			sum -= i
+			i++
+			column = column[1:]
+		} else if sum < target {
+			j++
+			sum += j
+			column = append(column, j)
+		} else {
+			ret = append(ret, column)
+			j++
+			sum += j - i
+			i++
+			column = column[1:]
+			column = append(column, j)
+		}
+	}
+	return ret
+}
