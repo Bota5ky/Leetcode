0c7c24fb38f4c9ff5292dd67e76497bbb963a6f2
Bota5ky <zjy2516@126.com>
2020-03-02 19:21:07 +0800

    面试题63. 股票的最大利润

---
+++ offer63.go
@@ -0,0 +1,17 @@
+package temp
+
+func maxProfit(prices []int) int {
+	minStack := make([]int, len(prices))
+	max := 0
+	for i := 0; i < len(prices); i++ {
+		if i == 0 || i > 0 && prices[i] < minStack[i-1] {
+			minStack[i] = prices[i]
+		} else {
+			minStack[i] = minStack[i-1]
+			if max < prices[i]-minStack[i] {
+				max = prices[i] - minStack[i]
+			}
+		}
+	}
+	return max
+}
