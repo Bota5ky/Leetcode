a11fa29b5f1b6b00a967877767ad5d05e47e4930
Bota5ky <zjy2516@126.com>
2020-03-03 19:57:40 +0800

    CI面试题 02.01. 移除重复节点

---
+++ interview02.01.go
@@ -0,0 +1,19 @@
+package temp
+
+//不使用额外空间的暴力方法 O(N^2)
+func removeDuplicateNodes2(head *ListNode) *ListNode {
+	ret := head
+	for head != nil {
+		pre := head
+		temp := head.Val
+		for pre.Next != nil {
+			if pre.Next.Val == temp {
+				pre.Next = pre.Next.Next
+			} else {
+				pre = pre.Next
+			}
+		}
+		head = head.Next
+	}
+	return ret
+}
