b75729e42ac352fb2246abcc3ab4ac70ff50abde
Bota5ky <zjy2516@126.com>
2020-03-03 12:24:45 +0800

    CI面试题 01.05. 一次编辑

---
+++ interview01.05.go
@@ -0,0 +1,16 @@
+package temp
+
+func oneEditAway(first string, second string) bool {
+	if len(first) == 0 {
+		return len(second) <= 1
+	}
+	if len(second) == 0 {
+		return len(first) <= 1
+	}
+	if first[0] == second[0] {
+		return oneEditAway(first[1:], second[1:])
+	} else if first[len(first)-1] == second[len(second)-1] {
+		return oneEditAway(first[:len(first)-1], second[:len(second)-1])
+	}
+	return len(first) == 1 && len(second) == 1
+}
