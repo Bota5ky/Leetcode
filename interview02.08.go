0468182ec21220a6f9bf27720361952c8b2e2f24
Bota5ky <zjy2516@126.com>
2020-03-04 16:20:45 +0800

    CI面试题 02.08. 环路检测

---
+++ interview02.08.go
@@ -0,0 +1,21 @@
+package temp
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast, rec := head, head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	fast = rec
+	for fast != slow {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return slow
+}
