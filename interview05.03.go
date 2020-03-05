6fa4b45a2521a1b35c953a5d9fd08d4d41c3992d
Bota5ky <zjy2516@126.com>
2020-03-05 21:07:31 +0800

    CI面试题 05.03. 翻转数位

---
+++ interview05.03.go
@@ -0,0 +1,21 @@
+package temp
+
+func reverseBits(num int) int {
+	max, n1, n2 := 0, 0, 0
+	for {
+		if num&1 == 1 {
+			n1++
+		} else {
+			if 1+n1+n2 > max {
+				max = 1 + n1 + n2
+			}
+			if num == 0 {
+				break
+			}
+			n2 = n1
+			n1 = 0
+		}
+		num >>= 1
+	}
+	return max
+}
