91647c6c2c7134e7e67a95be75c6eec79fe63cda
Bota5ky <zjy2516@126.com>
2020-03-05 21:48:10 +0800

    CI面试题 05.01. 插入

---
+++ interview05.01.go
@@ -0,0 +1,12 @@
+package temp
+
+func insertBits(N int, M int, i int, j int) int {
+	for i > 0 {
+		M <<= 1
+		M = M | ((N >> (i - 1)) & 1)
+		i--
+	}
+	N >>= (j + 1)
+	N <<= (j + 1)
+	return N | M
+}
