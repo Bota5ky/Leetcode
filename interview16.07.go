d9a2fc4dae185bafd13dac88ca54ca82a88b6599
Bota5ky <zjy2516@126.com>
2020-03-06 09:04:43 +0800

    CI面试题 16.07. 最大数值

---
+++ interview16.07.go
@@ -0,0 +1,8 @@
+package temp
+
+func maximum(a int, b int) int {
+	// abs(x) = ( x ^ (x >> 7)) - ( x >> 7 )
+	// max = ( a+b + |a-b| ) / 2
+	val := a - b
+	return (a + b + ((val ^ (val >> 63)) - (val >> 63))) / 2
+}
