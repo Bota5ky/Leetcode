cba981663109bbc6f511d2697cdc023bcdc0a703
Bota5ky <zjy2516@126.com>
2020-03-07 10:44:31 +0800

    CI面试题 16.17. 连续数列

---
+++ interview16.17.go
@@ -0,0 +1,15 @@
+package temp
+
+//比offer42严谨
+func maxSubArray2(nums []int) int {
+	maxSum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if maxSum < nums[i] {
+			maxSum = nums[i]
+		}
+	}
+	return maxSum
+}
