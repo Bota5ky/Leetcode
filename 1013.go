8d823ea9651bee24cc3e5dc399ff773fe77e49f1
Bota5ky <zjy2516@126.com>
2020-03-11 09:25:44 +0800

    将数组分成和相等的三个部分

---
+++ 1013.go
@@ -0,0 +1,33 @@
+package temp
+
+func canThreePartsEqualSum(A []int) bool {
+	sum := 0
+	for i := 0; i < len(A); i++ {
+		sum += A[i]
+	}
+	if sum%3 != 0 {
+		return false
+	}
+	want := sum / 3
+	cnt := 0
+	sum = 0
+	var i int
+	for i = 0; i < len(A); i++ {
+		sum += A[i]
+		if sum == want {
+			cnt++
+			sum = 0
+		}
+		if cnt == 2 {
+			i++
+			break
+		}
+	}
+	for ; i < len(A); i++ {
+		sum += A[i]
+		if sum == want && i == len(A)-1 {
+			return true
+		}
+	}
+	return false
+}
