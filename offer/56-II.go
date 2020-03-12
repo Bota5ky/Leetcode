711356d897056af3c17ca37e7491d5c4ae654a23
Bota5ky <zjy2516@126.com>
2020-03-12 12:30:35 +0800

    面试题56 - II. 数组中数字出现的次数 II

--- offer/56-II.go
+++ offer/56-II.go
@@ -1,5 +1,6 @@
-package temp
+package leetcode
 
+//https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/
 func singleNumber2(nums []int) int {
 	res := make([]int, 32)
 	for _, v := range nums {
