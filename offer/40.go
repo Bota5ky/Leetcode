100019e2898d0c48fdda79cb0659dc17759d4020
Bota5ky <zjy2516@126.com>
2020-03-12 12:25:01 +0800

    面试题40. 最小的k个数

--- offer/40.go
+++ offer/40.go
@@ -1,5 +1,6 @@
-package temp
+package leetcode
 
+//https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
 func getLeastNumbers(arr []int, k int) []int {
 	for i := k; i > 0; i-- {
 		for j := len(arr) - 2; j >= 0; j-- {
