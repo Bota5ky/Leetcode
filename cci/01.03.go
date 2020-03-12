d1ad4e5fb3d800bf3519563bef844057eb679539
Bota5ky <zjy2516@126.com>
2020-03-12 11:25:19 +0800

    面试题 01.03. URL化

--- cci/01.03.go
+++ cci/01.03.go
@@ -1,7 +1,8 @@
-package temp
+package leetcode
 
 import "strings"
 
+//https://leetcode-cn.com/problems/string-to-url-lcci/
 func replaceSpaces(S string, length int) string {
 	return strings.Replace(S[:length], " ", "%20", -1)
 }
