af1b099cb825c9c64d338ebad103bc021ccd2de0
Bota5ky <zjy2516@126.com>
2020-03-12 11:13:13 +0800

    1304. 和为零的N个唯一整数

--- 1304.go
+++ 1304.go
@@ -1,5 +1,6 @@
-package temp
-//find-n-unique-integers-sum-up-to-zero
+package leetcode
+
+//https://leetcode-cn.com/problems/find-n-unique-integers-sum-up-to-zero/
 func sumZero(n int) []int {
     var ret []int
     for i:=-n/2; i<=n/2; i++ {
