a5d06333973caba439a2b22f5ec0d0d27650f795
Bota5ky <zjy2516@126.com>
2020-03-12 12:27:00 +0800

    面试题46. 把数字翻译成字符串

--- offer/46.go
+++ offer/46.go
@@ -1,6 +1,7 @@
-package temp
+package leetcode
 
 //f[i]=f[i-1]+g(i,i-1)*f[i-2]
+//https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/
 func translateNum(num int) int {
 	f := []int{1, 1}
 	last := num % 10
