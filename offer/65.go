60a24598504e3d9843173afb056ad50faa749984
Bota5ky <zjy2516@126.com>
2020-03-12 12:36:31 +0800

    面试题65. 不用加减乘除做加法

--- offer/65.go
+++ offer/65.go
@@ -1,5 +1,6 @@
-package temp
+package leetcode
 
+//https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
 func add(a int, b int) int {
 	for b != 0 {
 		tmp := a ^ b     //不计进位的相加结果
