01fd0bd435ef98fc6a71c1af13d81a1070a00adc
Bota5ky <zjy2516@126.com>
2020-03-12 10:06:21 +0800

    CI面试题 04.08. 首个共同祖先

---
+++ interview04.08.go
@@ -0,0 +1,15 @@
+package temp
+
+//和236相同
+func lowestCommonAncestor3(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+    if root==nil {return nil}
+    if root==p {return p}
+    if root==q {return q}
+    left:=lowestCommonAncestor(root.Left,p,q)
+    right:=lowestCommonAncestor(root.Right,p,q)
+    if left!=nil && right!=nil {return root}
+    if left!=nil {
+        return left
+    }
+    return right
+}
