d331de67c7487184867d6e812d8e24daf2b32948
Bota5ky <zjy2516@126.com>
2020-03-12 11:23:59 +0800

    面试题 01.01. 判定字符是否唯一

--- cci/01.01.go
+++ cci/01.01.go
@@ -1,7 +1,8 @@
-package temp
+package leetcode
 
 import "strings"
 
+//https://leetcode-cn.com/problems/is-unique-lcci/
 func isUnique(astr string) bool {
 	for i := 0; i < len(astr); i++ {
 		if strings.Contains(astr[i+1:], string(astr[i])) {
