6e6c8491028fcc4245ea6e55da26f120e4dd5c60
Bota5ky <zjy2516@126.com>
2020-03-12 11:31:02 +0800

    面试题 01.09. 字符串轮转

--- cci/01.09.go
+++ cci/01.09.go
@@ -1,7 +1,8 @@
-package temp
+package leetcode
 
 import "strings"
 
+//https://leetcode-cn.com/problems/string-rotation-lcci/
 func isFlipedString(s1 string, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
