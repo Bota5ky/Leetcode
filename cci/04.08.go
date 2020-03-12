337db41d8cee9f371ec7db31dcf8169e0a491879
Bota5ky <zjy2516@126.com>
2020-03-12 12:01:18 +0800

    面试题 04.08. 首个共同祖先

--- cci/04.08.go
+++ cci/04.08.go
@@ -1,16 +1,24 @@
 package leetcode
 
-//https://leetcode-cn.com/problems/first-common-ancestor-lcci/
 //和236相同
+//https://leetcode-cn.com/problems/first-common-ancestor-lcci/
 func lowestCommonAncestor3(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
-    if root==nil {return nil}
-    if root==p {return p}
-    if root==q {return q}
-    left:=lowestCommonAncestor(root.Left,p,q)
-    right:=lowestCommonAncestor(root.Right,p,q)
-    if left!=nil && right!=nil {return root}
-    if left!=nil {
-        return left
-    }
-    return right
+	if root == nil {
+		return nil
+	}
+	if root == p {
+		return p
+	}
+	if root == q {
+		return q
+	}
+	left := lowestCommonAncestor3(root.Left, p, q)
+	right := lowestCommonAncestor3(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	return right
 }
