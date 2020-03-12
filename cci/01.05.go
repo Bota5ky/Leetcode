f36a0b3859992f7cfb08c0e8bbc222720d15103e
Bota5ky <zjy2516@126.com>
2020-03-12 11:26:19 +0800

    面试题 01.05. 一次编辑

--- cci/01.05.go
+++ cci/01.05.go
@@ -1,5 +1,6 @@
-package temp
+package leetcode
 
+//https://leetcode-cn.com/problems/one-away-lcci/
 func oneEditAway(first string, second string) bool {
 	if len(first) == 0 {
 		return len(second) <= 1
