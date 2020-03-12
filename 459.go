f3d91e3dba5ca0acb828f1ae44df8f0158114986
Bota5ky <zjy2516@126.com>
2020-03-12 11:00:02 +0800

    459. 重复的子字符串

--- 459.go
+++ 459.go
@@ -1,8 +1,8 @@
-package temp
+package leetcode
 
 import "strings"
 
-//repeated-substring-pattern
+//https://leetcode-cn.com/problems/repeated-substring-pattern/
 func repeatedSubstringPattern(s string) bool {
 	return strings.Contains((s + s)[1:len(s+s)-1], s)
 }
