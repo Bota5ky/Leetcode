2295144c7c5c403b1841e01401cc4b92d13b5989
Bota5ky <zjy2516@126.com>
2020-03-15 18:19:14 +0800

    23. 合并K个排序链表

---
+++ 23.go
@@ -0,0 +1,42 @@
+package leetcode
+
+//https://leetcode-cn.com/problems/merge-k-sorted-lists/
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	for len(lists) > 1 {
+		temp := make([]*ListNode, 0)
+		for i := 0; i < len(lists); i += 2 {
+			if i == len(lists)-1 {
+				temp = append(temp, lists[i])
+			} else {
+				temp = append(temp, mergeTwoLists(lists[i], lists[i+1]))
+			}
+		}
+		lists = temp
+	}
+	return lists[0]
+}
+
+func mergeTwoLists(a, b *ListNode) *ListNode {
+	pre := &ListNode{}
+	ret := pre
+	for a != nil && b != nil {
+		if a.Val < b.Val {
+			pre.Next = a
+			a = a.Next
+		} else {
+			pre.Next = b
+			b = b.Next
+		}
+		pre = pre.Next
+	}
+	if a != nil {
+		pre.Next = a
+	}
+	if b != nil {
+		pre.Next = b
+	}
+	return ret.Next
+}
