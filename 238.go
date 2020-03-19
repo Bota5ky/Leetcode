3564494e7c8de4d8c3d63911dcb065a453069490
Bota5ky <zjy2516@126.com>
2020-03-19 22:40:05 +0800

    238. 除自身以外数组的乘积

--- 238.go
+++ 238.go
@@ -2,15 +2,15 @@ package leetcode
 
 //https://leetcode-cn.com/problems/product-of-array-except-self/
 func productExceptSelf(nums []int) []int {
-	ret := make([]int, len(nums))
-	ret[0] = 1
-	for i := 0; i < len(nums)-1; i++ {
-		ret[i+1] = ret[i] * nums[i]
+	res := make([]int, len(nums))
+	res[0] = 1
+	for i := 1; i < len(nums); i++ {
+		res[i] = res[i-1] * nums[i-1]
 	}
-	temp := 1
+	temp := nums[len(nums)-1]
 	for i := len(nums) - 2; i >= 0; i-- {
-		temp *= nums[i+1]
-		ret[i] *= temp
+		res[i] *= temp
+		temp *= nums[i]
 	}
-	return ret
+	return res
 }
