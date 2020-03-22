593a51d739a554421459670dc2d3eac111fc3e09
Bota5ky <zjy2516@126.com>
2020-03-22 12:44:24 +0800

    148. 排序链表

--- 148.go
+++ 148.go
@@ -1,6 +1,7 @@
 package leetcode
 
 //https://leetcode-cn.com/problems/sort-list/
+//递归归并 时间复杂度 nlogn 空间复杂度 nlogn
 func sortList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
@@ -36,4 +37,45 @@ func mergeList(l1 *ListNode, l2 *ListNode) *ListNode {
 		pre.Next = l1
 	}
 	return ret.Next
+}
+
+//迭代归并 时间复杂度 nlogn 空间复杂度 O(1)
+func sortList2(head *ListNode) *ListNode {
+	pre, n := &ListNode{Next: head}, 0
+	for head != nil {
+		n++ //链表总长度
+		head = head.Next
+	}
+	for i := 1; i < n; i *= 2 {
+		prehead := pre
+		for prehead.Next != nil {
+			l1 := prehead.Next
+			cur := l1
+			for j := 0; j < i-1 && cur != nil; j++ {
+				cur = cur.Next
+			}
+			if cur == nil {
+				break
+			}
+			l2 := cur.Next
+			cur.Next = nil
+			cur = l2
+			for j := 0; j < i-1 && cur != nil; j++ {
+				cur = cur.Next
+			}
+			var l3 *ListNode
+			if cur != nil {
+				l3 = cur.Next
+				cur.Next = nil
+			}
+			node := mergeList(l1, l2)
+			prehead.Next = node
+			for node.Next != nil {
+				node = node.Next
+			}
+			node.Next = l3
+			prehead = node
+		}
+	}
+	return pre.Next
 }
