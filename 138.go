933990f01821ad38ffe5320cdfaae38f90eef731
Bota5ky <zjy2516@126.com>
2020-04-01 09:29:48 +0800

    138. 复制带随机指针的链表

--- 138.go
+++ 138.go
@@ -2,10 +2,13 @@ package leetcode
 
 //https://leetcode-cn.com/problems/copy-list-with-random-pointer/
 func copyRandomList(head *Node2) *Node2 {
+	if head == nil {
+		return nil
+	}
 	cur := head
 	for cur != nil {
-		newnode := &Node2{Val: cur.Val, Next: cur.Next}
-		cur.Next = newnode
+		newNode := &Node2{Val: cur.Val, Next: cur.Next}
+		cur.Next = newNode
 		cur = cur.Next.Next
 	}
 	cur = head
@@ -15,18 +18,19 @@ func copyRandomList(head *Node2) *Node2 {
 		}
 		cur = cur.Next.Next
 	}
-	if head != nil {
-		cur = head.Next
-	}
-	ret := cur
-	for cur != nil && cur.Next != nil {
-		cur.Next = cur.Next.Next
-		cur = cur.Next
+	cur = head
+	ret := cur.Next	//用完得复原
+	for cur != nil {
+		temp := cur.Next
+		if cur.Next != nil {
+			cur.Next = cur.Next.Next
+		}
+		cur = temp
 	}
 	return ret
 }
 
-//Node2 Node
+//Node2 Node2
 type Node2 struct {
 	Val    int
 	Next   *Node2
