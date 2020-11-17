1e669968e05928cf6c32c8af317c39792edd91a8
Bota5ky <zjy2516@126.com>
2020-11-17 15:29:35 +0800

    406. 根据身高重建队列

--- 406.java
+++ 406.java
@@ -1,24 +1,42 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 //https://leetcode-cn.com/problems/queue-reconstruction-by-height/
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, new Comparator<int[]>() {
-            public int compare(int[] person1, int[] person2) {
-                if (person1[0] != person2[0]) {
-                    return person2[0] - person1[0];
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o2[0] - o1[0];
                 } else {
-                    return person1[1] - person2[1];
+                    return o1[1] - o2[1];
                 }
             }
         });
-        List<int[]> ans = new ArrayList<int[]>();
-        for (int[] person : people) {
-            ans.add(person[1], person);
+        // for (int k = 0; k < people.length; k++) {
+        // System.out.printf("%d,%d;", people[k][0], people[k][1]);
+        // }
+        // System.out.println();
+        for (int i = 0; i < people.length; i++) {
+            int[] temp = new int[2];
+            temp[0] = people[i][0];
+            temp[1] = people[i][1];
+            for (int j = i - 1; j >= temp[1]; j--) {
+                people[j + 1] = people[j];
+            }
+            people[temp[1]] = temp;
+            // for (int k = 0; k < people.length; k++) {
+            // System.out.printf("%d,%d;", people[k][0], people[k][1]);
+            // }
+            // System.out.println();
         }
-        return ans.toArray(new int[ans.size()][]);
+        return people;
+    }
+
+    public static void main(String[] args) {
+        int[][] people = { { 7, 0 }, { 4, 4 }, { 7, 1 }, { 5, 0 }, { 6, 1 }, { 5, 2 } };
+        //ans [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+        Solution sol = new Solution();
+        sol.reconstructQueue(people);
     }
 }
