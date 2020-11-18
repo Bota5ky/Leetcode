bfa72bd76046e463516f10bd796f27b4fd2e6d75
Bota5ky <zjy2516@126.com>
2020-11-18 15:52:18 +0800

    2020年11月18日 15:52:18

---
+++ java/leetcode374.java
@@ -0,0 +1,32 @@
+package java;
+
+//https://leetcode-cn.com/problems/guess-number-higher-or-lower/
+public class leetcode374 extends GuessGame {
+    public int guessNumber(int n) {
+        for (int i = 1, j = n; i <= j;) {
+            int mid = i + (j - i) / 2;
+            if (guess(mid) < 0) {
+                j = mid - 1;
+            } else if (guess(mid) > 0) {
+                i = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+class GuessGame {
+    int n;
+
+    int guess(int num) {
+        if (num > n) {
+            return 1;
+        } else if (num < n) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
